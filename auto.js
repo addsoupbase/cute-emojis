b233efdc83c9c7e4a320a59b2fefe3f4bdb51a0b
addsoupbase <162063292+addsoupbase@users.noreply.github.com>
2025-01-18 20:35:21 -0500

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,3 +1,10 @@
-//Auto import images
-let all = JSON.stringify(await Array.fromAsync(Deno.readDir('./emojis'),o=>o.name))
-Deno.writeTextFile('./emojis.json',all)
+//Auto imports images
+try {
+   const all = await Array.fromAsync(Deno.readDir('./emojis'), o => o.name)
+   await Deno.writeTextFile('./emojis.json', JSON.stringify(all))
+   console.log(`✅ Successfully wrote output (${all.length} emojis) to emojis.json`)
+}
+catch (e) {
+   console.log('❌ Could not write output. Error is shown below')
+   reportError(e)
+}
