5d5f3533567b8329034431b60b866dee302ee86d
addsoupbase <162063292+addsoupbase@users.noreply.github.com>
2025-01-19 00:05:10 -0500

    Option to download all emojis

--- auto.js
+++ auto.js
@@ -1,10 +1,17 @@
 //Auto imports images
+let time = performance.now()
 try {
-   const all = await Array.fromAsync(Deno.readDir('./emojis'), o => o.name)
+   const all = await Array.fromAsync(Deno.readDir('./emojis'), map)
    await Deno.writeTextFile('./emojis.json', JSON.stringify(all))
    console.log(`✅ Successfully wrote output (${all.length} emojis) to emojis.json`)
 }
 catch (e) {
-   console.log('❌ Could not write output. Error is shown below')
+   console.log('❌ Could not write output. Error is shown below:')
    reportError(e)
+}
+finally {
+   console.log(`⏰ Operation completed after ${performance.now() - time} ms`)
+}
+function map({name}) {
+   return name
 }
